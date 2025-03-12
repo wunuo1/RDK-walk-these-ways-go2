6c99e2f41635f1f05f387fab6a6b3bcd27e39099
nuo.wu <57823827+wunuo1@users.noreply.github.com>
2025-03-12 14:44:29 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ setup(
     packages=find_packages(),
     author_email='540614105@qq.com',
     description='Toolkit for deployment of sim-to-real RL on the Unitree Go2.',
-    install_requires=['ml_logger==0.8.117',
-                      'ml_dash==0.3.20',
-                      'jaynes>=0.9.2',
+    install_requires=['ml_logger==0.10.35',
+                      'ml_dash==0.3.25',
+                      'jaynes==0.9.10',
                       'params-proto==2.10.5',
-                      'gym>=0.14.0',
-                      'tqdm',
-                      'matplotlib',
+                      'gym==0.26.2',
+                      'tqdm==4.67.1',
+                      'matplotlib==3.7.5',
                       'numpy==1.23.5'
                       ]
 )
